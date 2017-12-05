10b7165fdbef0a267e458c0ff62aac7fd168434a
Marcus <mfondon@salesforce.com>
2017-12-05 09:15:40 -0500

    Moving state logic to game component

--- tic-tac-toe-app/src/components/board.js
+++ tic-tac-toe-app/src/components/board.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import Square from './square.js'
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      playerTurn: 'X',
-    };
-  }
   renderSquare(i, winningSquares) {
     let isWinner = false;
     if (winningSquares) {
@@ -19,76 +12,34 @@ class Board extends React.Component {
     }
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick ={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick ={() => this.props.handleClick(i)}
         isWinner = {isWinner}
       />
     );
   }
 
-  handleClick(i) {
-    let squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    let playerTurn = this.state.playerTurn;
-    squares[i] = playerTurn;
-    this.setState({
-      squares: squares,
-      playerTurn: playerTurn === 'X' ? 'O' : 'X'});
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status
-    let winners = null;
-    if (winner) {
-      status = 'Winner: ' + winner['winner'];
-      winners = winner['squares'];
-    } else {
-      status = 'Next player: ' + this.state.playerTurn;      
-    }
+    let winningSquares = this.props.winningSquares;
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0, winners)}
-          {this.renderSquare(1, winners)}
-          {this.renderSquare(2, winners)}
+          {this.renderSquare(0, winningSquares)}
+          {this.renderSquare(1, winningSquares)}
+          {this.renderSquare(2, winningSquares)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3, winners)}
-          {this.renderSquare(4, winners)}
-          {this.renderSquare(5, winners)}
+          {this.renderSquare(3, winningSquares)}
+          {this.renderSquare(4, winningSquares)}
+          {this.renderSquare(5, winningSquares)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6, winners)}
-          {this.renderSquare(7, winners)}
-          {this.renderSquare(8, winners)}
+          {this.renderSquare(6, winningSquares)}
+          {this.renderSquare(7, winningSquares)}
+          {this.renderSquare(8, winningSquares)}
         </div>
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a]=== squares[b] && squares[a] === squares[c]) {
-      let winner = squares[a];
-      return {'winner': winner, 'squares': lines[i]};
-    }
-  }
-  return null;
 }
 export default Board;
--- tic-tac-toe-app/src/components/game.js
+++ tic-tac-toe-app/src/components/game.js
@@ -3,18 +3,69 @@ import ReactDOM from 'react-dom';
 import Board from './board.js'
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      playerTurn: 'X',
+      winner: null
+    };
+  }
+
+  handleClick(i) {
+    let squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    let playerTurn = this.state.playerTurn;
+    squares[i] = playerTurn;
+    this.setState({
+      squares: squares,
+      playerTurn: playerTurn === 'X' ? 'O' : 'X'});
+  }
+  
   render() {
+    const winStatus = calculateWinner(this.state.squares);
+    let status
+    let winningSquares = null;
+    if (winStatus) {
+      status = 'Winner: ' + winStatus['winner'];
+      winningSquares = winStatus['squares'];
+    } else {
+      status = 'Next player: ' + this.state.playerTurn;      
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+          <Board squares={this.state.squares} winningSquares={winningSquares} handleClick={(i) => this.handleClick(i)}/>
         </div>
       </div>
     );
   }
 }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a]=== squares[b] && squares[a] === squares[c]) {
+      let winner = squares[a];
+      return {'winner': winner, 'squares': lines[i]};
+    }
+  }
+  return null;
+}
+
 export default Game
